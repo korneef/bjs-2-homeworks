975271669282afdb57a183c863bc603a1eddfe6f
Valentin Korneev <v.korneef@gmail.com>
2021-08-12 22:57:26 +0500

    First variant. Jasmine 3 failures

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,19 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  "use strict"
+  arr = []
+  let d;
+  d = Math.pow(b, 2) - (4 * a * c);
+
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+  } else if (d < 0) {
+    arr = [];
+  }
   return arr; // array
 }
 
@@ -8,6 +21,29 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
+  "use strict"
 
+  if (isNaN(+percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+  } else if (isNaN(+contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+  } else if (isNaN(+amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+  };
+  percent = +percent;
+  contribution = +contribution;
+  amount = +amount;
+
+  let today = new Date;
+  let months;
+  months = (date.getFullYear() - today.getFullYear()) * 12;
+  months += date.getMonth();
+  months -= today.getMonth();
+  
+  let creditBody = amount - contribution;
+  percent = (percent/100/12);
+  let monthPaymet;
+  monthPaymet = creditBody * (percent + percent / (((1 + percent)**months) - 1));
+  totalAmount = +(monthPaymet * months).toFixed(2);
   return totalAmount;
 }
