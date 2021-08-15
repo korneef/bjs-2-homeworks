7517c46da8372e0f0f2cd668a63d3ffa27ff1e4a
Valentin Korneev <v.korneef@gmail.com>
2021-08-15 20:32:40 +0500

    Homework on the topic Functions to check

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,66 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;  
+  let avg;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    };
 
+    if (max < arr[i]) {
+      max = arr[i];
+    };
+
+    sum = sum + arr[i];  
+  };
+
+  avg = +(sum / arr.length).toFixed(2);
+  
   return { min:min, max:max, avg:avg };
 }
 
-// Задание 2
+//Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  };
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
-  return max
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let maxFunc = func (arrOfArr[i]);
+    if (maxFunc > max) {
+      max = maxFunc;
+    }
+  }
+
+
+  return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
-}
+  let min = Infinity;
+  let max = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    };
+
+    if (max < arr[i]) {
+      max = arr[i];
+    };
+  }
+
+  return Math.abs(max - min);
+};
