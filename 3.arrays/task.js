c70dea605d9cfda577b2782c710e9d47a44908b8
Valentin Korneev <v.korneef@gmail.com>
2021-08-17 07:17:11 +0500

    thir task is completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,16 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
-}
+  if (arr1.length === arr2.length) {
+    result = arr1.every((item, idx) => item === arr2[idx]);
+    return result;
+  };
+  return false;
+};
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(item => (item > 0 && item % 3 === 0)).map(item => item * 10);
 
-  return resultArr; // array
-}
+  return resultArr;
+};
